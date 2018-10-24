ec434742ebdcf11e8d45358ee68b121e21e6d864
chrishibby75 <chrishibby75@gmail.com>
2018-10-24 17:41:39 -0600

    on click

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#itemOne').on('click', function(){
+        $('#about').css('display', 'block');
+        $('#portfolio').css('display', 'none');
+        $('#contact').css('display', 'none');
+    })
+    $('#itemTwo').on('click', function(){
+        $('#about').css('display', 'none');
+        $('#portfolio').css('display', 'block');
+        $('#contact').css('displey', 'none');
+    })
+    $('#itemThree').on('click', function(){
+        $('#about').css('display', 'none');
+        $('#portfolio').css('display', 'none');
+        $('#contact').css('display', 'block');
+    })
+})
